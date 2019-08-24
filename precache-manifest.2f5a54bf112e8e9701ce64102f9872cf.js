5ac3572d3013c7abbc974ff2a101c08a797aaf0a
Rolleh <32469870+Rollo98@users.noreply.github.com>
2019-08-25 00:11:40 +0300

    Updates

---
+++ precache-manifest.2f5a54bf112e8e9701ce64102f9872cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1641656ac4e42678fabf",
+    "url": "/ReminderJs/static/css/main.cb969fe3.chunk.css"
+  },
+  {
+    "revision": "1641656ac4e42678fabf",
+    "url": "/ReminderJs/static/js/main.1641656a.chunk.js"
+  },
+  {
+    "revision": "fd571ac5d39199d095f6",
+    "url": "/ReminderJs/static/css/1.995dea27.chunk.css"
+  },
+  {
+    "revision": "fd571ac5d39199d095f6",
+    "url": "/ReminderJs/static/js/1.fd571ac5.chunk.js"
+  },
+  {
+    "revision": "b6dd2b0f582e1329528c",
+    "url": "/ReminderJs/static/js/runtime~main.b6dd2b0f.js"
+  },
+  {
+    "revision": "570ea53eaabe81ab9dca148e15118f6f",
+    "url": "/ReminderJs/index.html"
+  }
+];
